c9beb987e167911db6caf903fd072aa1aee86f4a
Telegramwallah14 <131813732+Telegramwallah14@users.noreply.github.com>
2024-09-26 19:37:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
         return default
 
 #main variables
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '26964838'))
+API_HASH = environ.get('API_HASH', '2d90f84f7c576852beac2865eee31077')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 USERNAME = environ.get('USERNAME', 'https://telegram.me/CodeXBro')
